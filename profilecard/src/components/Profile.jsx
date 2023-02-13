b8f09d1af3a74180bc47598d361a174d4d6af77d
HYE <jelly12paw@gmail.com>
2023-02-13 22:04:26 +0900

    create likebutton

--- profilecard/src/components/Profile.jsx
+++ profilecard/src/components/Profile.jsx
@@ -8,7 +8,21 @@ export default function Profile({image, name, title, isLogin, isLogout}) {
             <p>{title}</p>
             {isLogin && <span></span>}
             {isLogout && <span></span>}
+            <LikeBtn />
         </div>
     );
 }
 
+function LikeBtn() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Like {count}
+        </button>
+    );
+}
