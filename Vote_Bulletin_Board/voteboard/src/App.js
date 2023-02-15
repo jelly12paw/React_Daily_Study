d5a060a745baa8fa065918a680f208adb6340c56
HYE <jelly12paw@gmail.com>
2023-02-15 17:38:33 +0900

    feat: IdolCard components

--- Vote_Bulletin_Board/voteboard/src/App.js
+++ Vote_Bulletin_Board/voteboard/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import IdolCard from './conponents/IdolCard';
 
 function App() {
   return (
     <>
+      <IdolCard
+        idolname="NewJeans"
+        image="https://news.koreadaily.com/data/photo/2023/01/27/a950f672-5d73-4021-9359-d100ceac3111.jpg"
+        song="Ditto" 
+      />
     </>
   );
 }
---
+++ Vote_Bulletin_Board/voteboard/src/conponents/IdolCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function IdolCard({image, idolname, song, onClick, total}) {
+    const [cnt, setCnt] = useState(0);
+
+    let voteRate = 0;
+    if (total) { voteRate = total / cnt;}
+    
+    return (
+        <div className='idol-card'>
+            <img src={image} alt="" />
+            <div className='voterate'>{voteRate}<span> %</span></div>
+            <h1>{idolname} <span>{song}</span></h1>
+            <div className='vote'>
+                <p>득표수 : <span>{cnt}</span></p>
+                <button className='votebtn' onClick={() => {
+                    setCnt((prev) => prev + 1);
+                    onClick();
+                }}>
+                    <i class="bi bi-heart-fill"></i>
+                </button>
+            </div>
+        </div>
+    );
+}
+
