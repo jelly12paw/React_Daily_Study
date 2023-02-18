d0f9ddb0897622ad444614e517eb1f9403839c48
HYE <jelly12paw@gmail.com>
2023-02-18 20:07:39 +0900

    feat: toggle button

--- To_Do_List/todolist/src/App.js
+++ To_Do_List/todolist/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import ToDos from './components/ToDos';
 
 function App() {
+  const [showToDos, setShowToDos] = useState(0);
   return (
-    <>
-      <ToDos/>
-    </>
+    <div className='todos'>
+      {showToDos && <ToDos/>}
+      <button className='dateBtn' onClick={() => setShowToDos((show) => !show)}>
+        2023년 2월 18일 할 일
+      </button>
+    </div>
   );
 }
 
