61d2b43385135d4032640df27391ee3e35111da0
HYE <jelly12paw@gmail.com>
2023-02-18 20:58:57 +0900

    feat: ToDos component 기본 틀

--- To_Do_List/todolist/src/components/ToDos.jsx
+++ To_Do_List/todolist/src/components/ToDos.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function ToDos() {
     return (
-        <div>
-            
+        <div className='lists'>
+            <label className='doneList' htmlFor="done"><i class="bi bi-calendar-check"></i> 완료한 일들만 모아보기</label>
+            <input type="checkbox" name="" id="done" />
+            <ul>
+                <li>
+                    <i class="bi bi-clipboard"></i><i class="bi bi-clipboard-check"></i>
+                    <h3>타이틀</h3>
+                    <h4>내용내용내용</h4>
+                </li>
+            </ul>
         </div>
     );
 }
