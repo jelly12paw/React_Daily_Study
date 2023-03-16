9b5a75419f1cd48a72bf83e40c90cfed8efb8811
HYE <jelly12paw@gmail.com>
2023-03-16 14:49:17 +0900

    feat: components files

--- Microsoft_Clone_Coding/microsoft/src/App.js
+++ Microsoft_Clone_Coding/microsoft/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Carousel from './components/Carousel/Carousel'
+import ButtonIcon from './components/ButtonIcon/ButtonIcon'
+import Goods from './components/Goods/Goods';
+import Xbox from './components/Xbox/Xbox';
+import Business from './components/Business/Business';
+import TopBtn from './components/TopBtn/TopBtn';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar/>
+      <Carousel/>
+      <ButtonIcon/>
+      <Goods/>
+      <Xbox/>
+      <Business/>
+      <TopBtn/>
+      <Footer/>
+    </>
   );
 }
 
