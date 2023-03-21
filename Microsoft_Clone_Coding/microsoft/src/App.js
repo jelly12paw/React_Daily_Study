5dd2cc0660bc72b2bf6e748cae4769b687897fb3
HYE <jelly12paw@gmail.com>
2023-03-21 19:38:52 +0900

    feat: Xbox component

--- Microsoft_Clone_Coding/microsoft/src/App.js
+++ Microsoft_Clone_Coding/microsoft/src/App.js
@@ -4,6 +4,7 @@ import ButtonIcon from './components/ButtonIcon/ButtonIcon';
 import Goods from './components/Goods/Goods';
 import products from './components/Goods/products';
 import business from './components/Goods/business';
+import Xbox from './components/Xbox/Xbox';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <ButtonIcon/>
       <Goods data={products} isBusiness={false}/>
+      <Xbox/>
       <Goods data={business} isBusiness={true}/>
     </>
   );
--- Microsoft_Clone_Coding/microsoft/src/components/Xbox/Xbox.jsx
+++ Microsoft_Clone_Coding/microsoft/src/components/Xbox/Xbox.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Xbox.module.css'
 
 export default function Xbox() {
     return (
-        <div>
-            
+        <div className={styles.imagecard}>
+            <div className={styles.content}>
+                <div className={styles.title}>Xbox Series X</div>
+                <div className={styles.text}>제품 역사상 가장 빠르고 강력한 Xbox</div>
+                <button className={styles.btn}>지금 쇼핑하기</button>
+            </div>
         </div>
     );
 }
