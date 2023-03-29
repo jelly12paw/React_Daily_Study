2c28d885737f4930ecd3f46a450c92ad375edc52
HYE <jelly12paw@gmail.com>
2023-03-29 17:56:06 +0900

    feat: react router route

--- Youtube_Clone_Coding/youtube/src/index.js
+++ Youtube_Clone_Coding/youtube/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Videos from './pages/Videos/Videos';
+import VideoDetail from './pages/VideoDetail/VideoDetail';
+import NotFound from './pages/NotFound/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, element: <Videos />},
+      {path: '/videos', element: <Videos />},
+      {path: '/videos/:keyword', element: <Videos />},
+      {path: '/videos/watch/:videoId', element: <VideoDetail />},
+    ]},
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
